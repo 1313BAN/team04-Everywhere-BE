9bb9d39ebdaf5b5557906259d0bfc9d1d1057f7b
Kim Jiyoon <rlawldbs0911@naver.com>
2025-05-11 02:54:46 +0900

    refactor: 커스텀 예외 클래스명 변경

--- src/main/java/com/ssafy/enjoytrip/everywhere/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssafy/enjoytrip/everywhere/common/exception/GlobalExceptionHandler.java
@@ -1,16 +1,14 @@
 package com.ssafy.enjoytrip.everywhere.common.exception;
 
-import com.ssafy.enjoytrip.everywhere.common.constants.ErrorCode;
 import com.ssafy.enjoytrip.everywhere.common.dto.response.ApiResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<ApiResponse<String>> handleInvalidToken(InvalidTokenException ex) {
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ApiResponse<String>> handleInvalidToken(ApiException ex) {
         return ResponseEntity
                 .status(ex.getErrorCode().status())
                 .body(ApiResponse.error(ex.getErrorCode(), ex.getErrorCode().message()));
