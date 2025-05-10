bf9cf526480b6805425c7ac728eabccc53c0b115
rlawldbs0911 <rlawldbs0911@naver.com>
2025-05-10 22:12:41 +0900

    feat: 유저 레포 refreshtoken 업데이트 쿼리 작성

--- src/main/java/com/ssafy/enjoytrip/everywhere/user/repository/UserRepository.java
+++ src/main/java/com/ssafy/enjoytrip/everywhere/user/repository/UserRepository.java
@@ -2,8 +2,15 @@ package com.ssafy.enjoytrip.everywhere.user.repository;
 
 import com.ssafy.enjoytrip.everywhere.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends
         JpaRepository<User, String> {
     boolean existsByUserId(String userId);
+
+    @Modifying
+    @Query("update User u set u.refreshToken = :refreshToken where u.userId = :userId")
+    void updateRefreshToken(@Param("userId") String userId, @Param("refreshToken") String refreshToken);
 }
