26cc62f4c8588b8bd0f36b8f91b73f20a2f43c6f
Kim Jiyoon <rlawldbs0911@naver.com>
2025-05-11 14:04:50 +0900

    fix: 게시글 수정 시 작성자 검증 추가

--- src/main/java/com/ssafy/enjoytrip/everywhere/user/entity/UserEntity.java
+++ src/main/java/com/ssafy/enjoytrip/everywhere/user/entity/UserEntity.java
@@ -11,12 +11,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Getter
+@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserEntity extends BaseEntity {
 
