a03577cbc6aa82cd0dcdefd8bf03ee3d93a00a25
김지윤 <rlawldbs0911@naver.com>
2025-05-12 13:40:32 +0900

    feat: WebConfig 작성

---
+++ src/main/java/com/ssafy/enjoytrip/everywhere/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ssafy.enjoytrip.everywhere.common.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://<프론트 실행되는 IP>:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
